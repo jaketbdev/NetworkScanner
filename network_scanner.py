bb7d78ef396c078eb440cfec5d4c0c6960f0952b
jaketbdev <72199858+jaketbdev@users.noreply.github.com>
2021-05-21 15:29:13 +0100

    Added dst mac address and created request to be sent

--- network_scanner.py
+++ network_scanner.py
@@ -4,6 +4,9 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request_broadcast
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 scan(ip)
