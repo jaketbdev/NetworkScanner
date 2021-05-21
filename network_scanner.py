dc75afadd3d2cd3649b44ca44826d80064ea95db
jaketbdev <72199858+jaketbdev@users.noreply.github.com>
2021-05-21 15:40:51 +0100

    Sent out the ARP request on the selected mac address and printed out the answered requests

--- network_scanner.py
+++ network_scanner.py
@@ -6,7 +6,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request_broadcast
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 scan(ip)
